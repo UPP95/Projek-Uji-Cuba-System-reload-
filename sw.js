a0ad60383f14479e9676acc553f04cacd083adea
UPP95 <mohdalifie16@gmail.com>
2026-09-14 00:33:07 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'topup-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
